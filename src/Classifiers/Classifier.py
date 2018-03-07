f7c0f48c2756d7c8dde58049efd3f60dc8a9e054
Omid <os14526@uga.edu>
2018-03-07 12:11:37 -0500

    adds FCN implementation in the Classifier OO Model

--- src/Classifiers/Classifier.py
+++ src/Classifiers/Classifier.py
@@ -36,7 +36,7 @@ class Classifier :
         logging.info("Loaded Model at : " + os.path.join(import_path, self.classifier_name + ".h5"))
         return self.trained_model
 
-    def train(self ):
+    def train(self, x_train , y_train  ):
         """
         this funciton  trains the model which is defined in it's body and
         saves the model in the class for further prediction
