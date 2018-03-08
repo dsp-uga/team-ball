a592adc316fac5c196a5c7acd2acffab695669a3
Parya Jandaghi <parya.jandaghi@gmail.com>
2018-03-08 11:36:47 -0500

    fix unet code

--- util/unet.py
+++ util/unet.py
@@ -1,3 +1,37 @@
+import numpy as np
+import os
+import tensorflow as tf
+from keras.models import Model
+
+from keras.layers import Input, merge, concatenate, Conv2D, MaxPooling2D, Activation, UpSampling2D, Dropout, Conv2DTranspose, UpSampling2D, Lambda
+from keras.layers.normalization import BatchNormalization as bn
+from keras.layers.merge import add
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.optimizers import RMSprop
+from keras import regularizers
+from keras import backend as K
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras.layers.merge import add
+import numpy as np
+from keras.regularizers import l2
+import cv2
+import glob
+import h5py
+
+
+smooth = 1.
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+
+    intersection = K.sum(y_true_f * y_pred_f )
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+
 
 def UNet(input_shape,learn_rate=1e-3):
     l2_lambda = 0.0002
@@ -5,72 +39,110 @@ def UNet(input_shape,learn_rate=1e-3):
     kernel_size=3
 
     inputs = Input(input_shape)
+    input_prob=Input(input_shape)
+    input_prob_inverse=Input(input_shape)
+    #Conv3D(filters,(3,3,3),sjfsjf)
+    conv1 = Conv2D( 32, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(inputs)
+    conv1 = bn()(conv1)
+    conv1 = Conv2D(32, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv1)
+    conv1 = bn()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    pool1 = Dropout(DropP)(pool1)
+
+
+
+
+
+    conv2 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(pool1)
+    conv2 = bn()(conv2)
+    conv2 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv2)
+    conv2 = bn()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    pool2 = Dropout(DropP)(pool2)
+
+
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(pool2)
+    conv3 = bn()(conv3)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv3)
+    conv3 = bn()(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    pool3 = Dropout(DropP)(pool3)
+
+
+
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(pool3)
+    conv4 = bn()(conv4)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv4)
+    conv4 = bn()(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    pool4 = Dropout(DropP)(pool4)
+
+
+
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(pool4)
+    conv5 = bn()(conv5)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv5)
+    conv5 = bn()(conv5)
     
-    # Block1 
-    conv1_1 = Conv2D( 64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(inputs)
-    conv1_2 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv1_1)
-    pool_1 = MaxPooling2D(pool_size=(2, 2))(conv1_2)
-    drop_1 = Dropout(DropP)(pool_1)
+    up6 = concatenate([Conv2DTranspose(256,(2, 2), strides=(2, 2), padding='same')(conv5), conv4],name='up6', axis=3)
+    up6 = Dropout(DropP)(up6)
+    conv6 = Conv2D(256,(3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(up6)
+    conv6 = bn()(conv6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv6)
 
+    conv6 = bn()(conv6)
+    up7 = concatenate([Conv2DTranspose(128,(2, 2), strides=(2, 2), padding='same')(conv6), conv3],name='up7', axis=3)
+    up7 = Dropout(DropP)(up7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(up7)
+    conv7 = bn()(conv7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv7)
+    conv7 = bn()(conv7)
 
-    # Block2
-    conv2_1 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(drop_1)
-    conv2_2 = Conv2D(128, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv2_1)
-    pool_2 = MaxPooling2D(pool_size=(2, 2))(conv2_2)
-    drop_2 = Dropout(DropP)(pool_2)
+    up8 = concatenate([Conv2DTranspose(64,(2, 2), strides=(2, 2), padding='same')(conv7), conv2],name='up8', axis=3)
+    up8 = Dropout(DropP)(up8)
+    conv8 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(up8)
+    conv8 = bn()(conv8)
+    conv8 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv8)
+    conv8 = bn()(conv8)
 
+    up9 = concatenate([Conv2DTranspose(32,(2, 2), strides=(2, 2), padding='same')(conv8), conv1],name='up9',axis=3)
+    up9 = Dropout(DropP)(up9)
+    conv9 = Conv2D(32, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(up9)
+    conv9 = bn()(conv9)
+    conv9 = Conv2D(32, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv9)
+    conv9 = bn()(conv9)   
 
-    # Block3
-    conv3_1 = Conv2D(128, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(drop_2)
-    conv3_2 = Conv2D(256, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv3_1)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3_2)
-    drop_3 = Dropout(DropP)(pool_3)
-
-
-    # Block4
-    conv4_1 = Conv2D(256, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(drop_3)
-    conv4_2 = Conv2D(512, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv4_1)
-    pool_4 = MaxPooling2D(pool_size=(2, 2))(conv4_2)
-    drop_4 = Dropout(DropP)(pool_4)
-
-
-    # Block5
-    conv5_1 = Conv2D(512, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(drop_4)
-    conv5_2 = Conv2D(1024, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv5_1)
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid',name='conv10')(conv9)
     
-    # Block6
-    up6 = concatenate([Conv2DTranspose(512,(2, 2), strides=(2, 2), padding='same')(conv5_2), conv4_2],name='up6', axis=3)
-    drop_6= Dropout(DropP)(up6)
-    conv6_1 = Conv2D(512,(3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(drop_6)
-    conv6_2 = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv6_1)
-
-    # Block7               
-    up7 = concatenate([Conv2DTranspose(256,(2, 2), strides=(2, 2), padding='same')(conv6_2), conv3_2],name='up7', axis=3)
-    drop_7 = Dropout(DropP)(up7)
-    conv7_1 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(drop_7)
-    conv7_2 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv7_1)
-
-    # Block8 
-    up8 = concatenate([Conv2DTranspose(128,(2, 2), strides=(2, 2), padding='same')(conv7_2), conv2_2],name='up8', axis=3)
-    drop_8 = Dropout(DropP)(up8)
-    conv8_1 = Conv2D(128, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(drop_8)
-    conv8_2 = Conv2D(128, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv8_1)
-
-
-    # Block9
-    up9 = concatenate([Conv2DTranspose(64,(2, 2), strides=(2, 2), padding='same')(conv8_2), conv1_2],name='up9',axis=3)
-    drop_9 = Dropout(DropP)(up9)
-    conv9_1 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(drop_9)
-    conv9_2 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_lambda) )(conv9_1)
-    conv9_3 = Conv2D(2, (1, 1), activation='sigmoid',name='conv9_3')(conv9_2)
-    
-
-    model = Model(inputs=[inputs], outputs=[conv9_3])
-    model.compile(optimizer=Adam(lr=1e-5), loss={'conv10': neg_dice_coef_loss, 'xxconv10': 'mse',})
+    model = Model(inputs=[inputs], outputs=[conv10])
+    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss)
     return model
 
 
-model=UNet(input_shape=(256,256,1))
+X_train=np.load('X_train.npy')
+y_train=np.load('Y_train.npy')
+X_test=np.load('X_test.npy')
+
+X_train=X_train.reshape(X_train.shape+(1,))
+y_train=y_train.reshape(y_train.shape+(1,))
+X_test=X_test.reshape(X_test.shape+(1,))
+
+model=UNet(input_shape=(512,512,1))
 print(model.summary())
+
+print(X_train.shape)
+print(y_train.shape)
+
+model.fit([X_train], [y_train], batch_size=4, epochs=1024, shuffle=True)
+model.save('ds-unet-batch8-epoch1024.h5')
+
+predicted = model.predict(X_test)
+for i in range(0,len(X_test)):
+    img_temp =   predicted[i] * 255 #cv2.normalize( predicted[i] , alpha=0, beta=1 , norm_type=cv2.NORM_MINMAX, datatype=cv2.CV_32F )
+    cv2.imwrite("output/"+str(i)+".png",img_temp)
+
+print('done')
+print(predicted.shape)
 
     
